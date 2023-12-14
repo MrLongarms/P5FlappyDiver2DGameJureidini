c49347a76a0766266628ccc14cc9c688bd5024a4
MrLongarms <704718@student.dusd.net>
2023-12-14 12:50:34 -0800

    Part 7 is Done!

--- FlappyDiver2DGameP5Juridini/Assets/Scripts/RepeatingBackground.cs
+++ FlappyDiver2DGameP5Juridini/Assets/Scripts/RepeatingBackground.cs
@@ -11,11 +11,20 @@ public class RepeatingBackground : MonoBehaviour
     void Start()
     {
         groundCollider = GetComponent<BoxCollider2D> ();
+        groundHorizontallength = groundCollider.size.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.x < -groundHorizontallength) 
+        {
+            RepositionBackground ();
+        }
+    }
+    private void RepositionBackground()
+    {
+        Vector2 groundOffset = new Vector2 (groundHorizontallength * 2f, 0);
+        transform.position = (Vector2) transform.position + groundOffset;
     }
 }
